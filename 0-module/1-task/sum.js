2ea0faa384042bd2e59ab4e1242a264844e8c20b
Ivan Vorobev <kudo82@mail.ru>
2020-03-05 23:09:52 +0300

    Задача 1. Сложение двух чисел

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+  return [a, b].map(value => {
+    if (typeof value !== "number") {
+      throw new TypeError("Value must be a number, a '"+(typeof value)+"' is passed");
+    }
+    return value;
+  }).reduce((a, b) => a + b, 0);
 }
 
 module.exports = sum;
